6fde3ea65a7cfa23c8178a907b363155637f7bd1
liaojian <liaojian@liaojian>
2016-09-27 11:35:18 +0800

    added JedisClient to indicate client end in the server end.

---
+++ src/jedis/util/JedisClient.java
@@ -0,0 +1,36 @@
+package jedis.util;
+
+public class JedisClient{
+	private String address;
+	int currentDB;
+	public JedisClient(String address) {
+		this.currentDB = 0;
+		this.address = address;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if(o == this) return true;
+		if(!(o instanceof JedisClient)) return false;
+		JedisClient client = (JedisClient)o;
+		return client.currentDB == this. currentDB &&
+				client.address.equals(this.address);
+	}
+	
+	@Override
+	public int hashCode(){
+		int hash = 23;
+		hash = 37*hash + this.currentDB;
+		hash = 37*hash + this.address.hashCode();
+		return hash;
+	}
+	
+	@Override
+	public String toString(){
+		return this.address + ":" + Integer.toString(this.currentDB);
+	}
+	
+	public void setCurrentDB(int newDB){
+		this.currentDB = newDB;
+	}
+}
