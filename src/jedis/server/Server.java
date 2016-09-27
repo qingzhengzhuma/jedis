221e8358be7a23111dfba61435a905d553d75550
liaojian <961617779@qq.com>
2016-09-27 22:45:13 +0800

    added command verification.

--- src/jedis/server/Server.java
+++ src/jedis/server/Server.java
@@ -12,36 +12,56 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-import jedis.util.JedisDB;
 import jedis.util.CommandHandler;
+import jedis.util.CommandLine;
+import jedis.util.GetHandler;
 import jedis.util.JedisClient;
+import jedis.util.JedisDB;
+import jedis.util.PingHandler;
+import jedis.util.SelectHandler;
+import jedis.util.SetHandler;
 
 public class Server {
-	
 	private Selector serverSelector;
-	private Map<String,SocketChannel> clientSockets;
+	private Map<String, SocketChannel> clientSockets;
 	private Map<String, JedisClient> clients;
 	private Map<String, CommandHandler> commandTable;
 	private ServerSocketChannel serverSocketChannel;
+
 	private int LISTEN_PORT = 8081;
 	private boolean isStop = false;
 	private int databaseNum = 16;
 	private JedisDB[] databases;
-	public Server(){
-		
+
+	private CommandEntry[] supportedCommandDict = { new CommandEntry("ping", new PingHandler()),
+			new CommandEntry("select", new SelectHandler()), new CommandEntry("get", new GetHandler()),
+			new CommandEntry("set", new SetHandler()) };
+
+	public Server() {
+
 	}
-	
-	private int loadConfigration(){
-		int port = 8081;
-		return port;
+
+	private void loadConfigration() {
+
 	}
-	
-	public boolean init(){
+
+	private void initCommandTable() {
+		commandTable = new HashMap<>();
+		for (CommandEntry ce : supportedCommandDict) {
+			commandTable.put(ce.command, ce.handler);
+		}
+	}
+
+	public boolean init() {
 		try {
 			loadConfigration();
 			databases = new JedisDB[databaseNum];
+			for (int i = 0; i < databaseNum; ++i) {
+				databases[i] = new JedisDB();
+			}
 			clientSockets = new HashMap<>();
-			commandTable = new HashMap<>();
+			clients = new HashMap<>();
+			initCommandTable();
 			serverSelector = Selector.open();
 			serverSocketChannel = ServerSocketChannel.open();
 			serverSocketChannel.socket().bind(new InetSocketAddress(LISTEN_PORT));
@@ -49,14 +69,14 @@ public class Server {
 			serverSocketChannel.register(serverSelector, SelectionKey.OP_ACCEPT);
 		} catch (IOException e) {
 			System.exit(-1);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			// TODO: handle exception
 			System.exit(-1);
 		}
 		return true;
 	}
-	
-	private void removeClient(SocketChannel clientChannel){
+
+	private void removeClient(SocketChannel clientChannel) {
 		try {
 			String address = clientChannel.getRemoteAddress().toString();
 			clientSockets.remove(address);
@@ -65,23 +85,23 @@ public class Server {
 			e.printStackTrace();
 		}
 	}
-	
-	private void handleAcception(SelectionKey key){
-		ServerSocketChannel server = (ServerSocketChannel)key.channel();
+
+	private void handleAcception(SelectionKey key) {
+		ServerSocketChannel server = (ServerSocketChannel) key.channel();
 		try {
 			SocketChannel socketChannel = server.accept();
 			socketChannel.configureBlocking(false);
-			socketChannel.register(serverSelector,SelectionKey.OP_READ);
+			socketChannel.register(serverSelector, SelectionKey.OP_READ);
 			String address = getRemoteAddress(socketChannel);
-			clientSockets.put(address,socketChannel);
+			clientSockets.put(address, socketChannel);
 			clients.put(address, new JedisClient(address));
 			System.out.println("Received Connection From " + address);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private String getRemoteAddress(SocketChannel socketChannel){
+
+	private String getRemoteAddress(SocketChannel socketChannel) {
 		try {
 			return socketChannel.getRemoteAddress().toString();
 		} catch (IOException e) {
@@ -90,49 +110,35 @@ public class Server {
 			return new String();
 		}
 	}
-	
-	private String parseCommand(byte[] data){
-		int length = data == null ? 0 : data.length;
-		int i = 0;
-		while(i < length && Character.isWhitespace(data[i++]));
-		StringBuilder stringBuilder = new StringBuilder();
-		while(i < length && Character.isWhitespace(data[i])) {
-			stringBuilder.append(data[i++]);
-		}
-		return stringBuilder.toString();
+
+	private boolean isValidCommand(String command) {
+		return commandTable.containsKey(command);
 	}
-	
-	private boolean isValidCommand(String command){
-		return command != null && 
-				command.length() > 0 && 
-				commandTable.containsKey(command);
-	}
-	
-	private byte[] processCommand(SocketChannel clientChannel,byte[] data){
-		String command = parseCommand(data);
-		if(!isValidCommand(command)) return "Unkonwn Command".getBytes();
-		String address = getRemoteAddress(clientChannel);
-		if(clientSockets.containsKey(address) && clients.containsKey(address)){
+
+	private byte[] processCommand(SocketChannel clientChannel, byte[] data) {
+		if (CommandLine.parse(data)) {
+			String command = CommandLine.getCommand();
+			if (isValidCommand(command) == false)
+				return "Unkonwn Command".getBytes();
+			String address = getRemoteAddress(clientChannel);
 			try {
 				CommandHandler handler = commandTable.get(command);
-				byte[] result = handler.execute(databases,
-						clients.get(address),
-						new String(data)).getBytes();
+				byte[] result = handler.execute(databases, clients.get(address), new String(data)).getBytes();
 				return result;
 			} catch (UnsupportedOperationException e) {
 				// TODO: handle exception
 				return "Not Supported Operation".getBytes();
 			}
 		}
-		return new byte[0];
+		return "ILLIGAL COMMAND".getBytes();
 	}
-	
-	private boolean sendResponse(SocketChannel clientChannel,byte[] result){
+
+	private boolean sendResponse(SocketChannel clientChannel, byte[] result) {
 		int length = result.length;
 		ByteBuffer buffer = ByteBuffer.allocate(length);
 		buffer.put(result);
 		buffer.flip();
-		while(buffer.hasRemaining()){
+		while (buffer.hasRemaining()) {
 			try {
 				clientChannel.write(buffer);
 			} catch (IOException e) {
@@ -143,38 +149,37 @@ public class Server {
 		}
 		return true;
 	}
-	
-	private void handleCommand(SelectionKey key){
+
+	private void handleCommand(SelectionKey key) {
 		SocketChannel clientChannel = (SocketChannel) key.channel();
 		ByteBuffer buffer = ByteBuffer.allocate(1024);
 		try {
 			int size = clientChannel.read(buffer);
-			if(size == -1){
+			if (size == -1) {
 				removeClient(clientChannel);
-			}else{
+			} else {
 				buffer.flip();
 				byte[] result = processCommand(clientChannel, buffer.array());
-				System.out.println(new String(result));
-				sendResponse(clientChannel,result);
+				sendResponse(clientChannel, result);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			buffer.clear();
 		}
 	}
-	
-	private void run(){
-		while(!isStop){
+
+	private void run() {
+		while (!isStop) {
 			try {
 				serverSelector.select();
 				Set<SelectionKey> selecedKeys = serverSelector.selectedKeys();
 				Iterator<SelectionKey> iterator = selecedKeys.iterator();
-				while(iterator.hasNext()){
+				while (iterator.hasNext()) {
 					SelectionKey key = iterator.next();
-					if(key.isAcceptable()){
+					if (key.isAcceptable()) {
 						handleAcception(key);
-					}else if(key.isReadable()){
+					} else if (key.isReadable()) {
 						handleCommand(key);
 					}
 				}
@@ -184,7 +189,7 @@ public class Server {
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Server server = new Server();
 		server.init();
