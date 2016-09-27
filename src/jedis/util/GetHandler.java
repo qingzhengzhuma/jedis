0b305b5f5f721a2630d55c6021e78d21fa3885cd
liaojian <961617779@qq.com>
2016-09-27 14:57:33 +0800

    added "get" command handler.

---
+++ src/jedis/util/GetHandler.java
@@ -0,0 +1,18 @@
+package jedis.util;
+
+public class GetHandler implements CommandHandler {
+	
+	private String parseKey(String command){
+		String key = command.split("\\s")[1];
+		return key;
+	}
+
+	@Override
+	public JedisObject execute(JedisDB[] database, JedisClient client, String command) {
+		// TODO Auto-generated method stub
+		String key = parseKey(command);
+		JedisDB db = database[client.currentDB];
+		return db.get(key);
+	}
+
+}
