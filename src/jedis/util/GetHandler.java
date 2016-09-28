c823d024ff163de9c4d8eb8a3a018470f9345a75
liaojian <961617779@qq.com>
2016-09-28 10:28:29 +0800

    improve the handler to process illegal arguements.

--- src/jedis/util/GetHandler.java
+++ src/jedis/util/GetHandler.java
@@ -1,18 +1,27 @@
 package jedis.util;
 
+import org.junit.internal.builders.NullBuilder;
+
+import jedis.util.SetHandler.Entry;
+
 public class GetHandler implements CommandHandler {
 	
 	private String parseKey(String command){
-		String key = command.split("\\s+")[1];
+		String key = command.split(" ")[1];
 		return key;
 	}
 
 	@Override
 	public JedisObject execute(JedisDB[] databases, JedisClient client, String command) {
 		// TODO Auto-generated method stub
-		String key = parseKey(command);
 		JedisDB db = databases[client.currentDB];
-		return db.get(key);
+		String key = parseKey(command);
+		if(db.containsKey(key)){
+			JedisObject object = db.get(key);
+			if(!(object instanceof Sds)) throw new UnsupportedOperationException();
+			else return (Sds)object;
+		}
+		return null;
 	}
 
 }
--- src/jedis/util/SelectHandler.java
+++ src/jedis/util/SelectHandler.java
@@ -3,11 +3,20 @@ package jedis.util;
 public class SelectHandler implements CommandHandler {
 	private int parseDBNum(String command) {
 		// TODO Auto-generated method stub
-		return 1;
+		String[] args = command.split(" ");
+		try{
+			int dbNum = Integer.parseInt(args[1]);
+			return dbNum;
+		}catch (NumberFormatException e) {
+			// TODO: handle exception
+			throw new IllegalArgumentException();
+		}
+		
 	}
 	
 	@Override
-	public JedisObject execute(JedisDB[]database,JedisClient client,String command) {
+	public JedisObject execute(JedisDB[]database,JedisClient client,String command) 
+			throws UnsupportedOperationException,IllegalArgumentException{
 		int dbNum = parseDBNum(command);
 		if(database == null || database.length == 0 || 
 				dbNum < 0 || dbNum >= database.length){
