251406966033d30878ed650ea79c5cd609ab9174
liaojian <961617779@qq.com>
2016-09-28 09:06:43 +0800

    associated to the handler.

---
+++ src/jedis/util/CommandRule.java
@@ -0,0 +1,32 @@
+package jedis.util;
+
+public class CommandRule{
+	private final String command;
+	private final int minArgc;
+	private final int maxArgc;
+	CommandHandler handler;
+	public CommandRule(String command,int minArgc,int maxArgc,CommandHandler handler) {
+		// TODO Auto-generated constructor stub
+		this.command = command;
+		this.minArgc = minArgc;
+		this.maxArgc = maxArgc;
+		this.handler = handler;
+	}
+	
+	public String getCommand() {
+		return this.command;
+	}
+	
+	public int getMinArgc(){
+		return this.minArgc;
+	}
+	
+	public int getMaxArgc(){
+		return this.maxArgc;
+	}
+	
+	public CommandHandler getHandler() {
+		return this.handler;
+		
+	}
+}
