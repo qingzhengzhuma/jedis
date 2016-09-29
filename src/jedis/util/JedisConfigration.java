7bbd3c2af7a72b8782ed173c8e31b3d06f89990a
liaojian <961617779@qq.com>
2016-09-29 09:41:41 +0800

    added the configuration item.

--- src/jedis/util/JedisConfigration.java
+++ src/jedis/util/JedisConfigration.java
@@ -2,12 +2,15 @@ package jedis.util;
 
 import jedis.util.CommandRule;
 
-public class CommandConfigration {
+public class JedisConfigration {
 	private static final CommandRule[] commandRules = {
 			new CommandRule("ping", 0, 0,new PingHandler()),
 			new CommandRule("select", 1, 1,new SelectHandler()),
 			new CommandRule("set", 2, 2,new SetHandler()),
-			new CommandRule("get", 1, 1,new GetHandler())
+			new CommandRule("get", 1, 1,new GetHandler()),
+			new CommandRule("exists", 1, 1, new ExistsHandler()),
+			new CommandRule("append", 2, 2, new AppendHandler()),
+			new CommandRule("del", 1, Integer.MAX_VALUE, new DeleteHandle())
 		};
 	
 	public static CommandRule[] getCommandRules(){
@@ -15,11 +18,22 @@ public class CommandConfigration {
 	}
 	
 	public static boolean verifyCommand(String command,int argc){
-		for(CommandRule rule : CommandConfigration.getCommandRules()){
+		for(CommandRule rule : JedisConfigration.getCommandRules()){
 			if(command.equals(rule.getCommand()) && 
 					argc >= rule.getMinArgc() && 
 					argc <= rule.getMaxArgc()) return true;
 		}
 		return false;
 	}
+	
+	public static JedisObjectReaderWriter[] getObjectReaders(){
+		JedisObjectReaderWriter[]  readers = {
+				new SdsReaderWriter(),
+		};
+		return readers;
+	}
+	
+	public static final String workPath = "/home/liaojian/workspace/";
+	public static final String rdbFileName = "jedis.rdb";
+	public static final String aofFileName = "jedis.aof";
 }
