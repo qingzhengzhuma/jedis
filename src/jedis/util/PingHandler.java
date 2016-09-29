ed94c3d66ed05b8f0b1473b01cb53773a9bd11ba
liaojian <961617779@qq.com>
2016-09-29 09:46:34 +0800

    updated to use MessageConstant.

--- src/jedis/util/PingHandler.java
+++ src/jedis/util/PingHandler.java
@@ -1,7 +1,7 @@
 package jedis.util;
 
 public class PingHandler implements CommandHandler{
-	public JedisObject execute(JedisDB[]database,JedisClient client,String command){
-		return new Sds("PONG");
+	public JedisObject execute(JedisDB[]database,JedisClient client,CommandLine cl){
+		return MessageConstant.PONG;
 	}
 }
--- src/jedis/util/SelectHandler.java
+++ src/jedis/util/SelectHandler.java
@@ -1,29 +1,23 @@
 package jedis.util;
 
 public class SelectHandler implements CommandHandler {
-	private int parseDBNum(String command) {
-		// TODO Auto-generated method stub
-		String[] args = command.split(" ");
+	
+	@Override
+	public JedisObject execute(JedisDB[]database,JedisClient client,CommandLine cl) 
+			throws UnsupportedOperationException,IllegalArgumentException{
+		
 		try{
-			int dbNum = Integer.parseInt(args[1]);
-			return dbNum;
+			int dbIndex = Integer.parseInt(cl.getArg(0));
+			if(database == null || database.length == 0 || 
+					dbIndex < 0 || dbIndex >= database.length){
+				return MessageConstant.INDEX_OUT_OF_BOUNDARY;
+			}
+			client.setCurrentDB(dbIndex);
+			return MessageConstant.OK;
 		}catch (NumberFormatException e) {
 			// TODO: handle exception
 			throw new IllegalArgumentException();
 		}
-		
-	}
-	
-	@Override
-	public JedisObject execute(JedisDB[]database,JedisClient client,String command) 
-			throws UnsupportedOperationException,IllegalArgumentException{
-		int dbNum = parseDBNum(command);
-		if(database == null || database.length == 0 || 
-				dbNum < 0 || dbNum >= database.length){
-			return new Sds("ERROR");
-		}
-		client.setCurrentDB(parseDBNum(command));
-		return new Sds("OK");
 	}
 
 }
