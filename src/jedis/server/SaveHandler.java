f1d374515348dcc0c3031d6d6bb77957d54f2b52
liaojian <961617779@qq.com>
2016-09-30 23:11:29 +0800

    moved from util package

--- src/jedis/server/SaveHandler.java
+++ src/jedis/server/SaveHandler.java
@@ -1,7 +1,6 @@
 package jedis.server;
 
 import jedis.util.CommandLine;
-import jedis.util.JedisClient;
 import jedis.util.JedisConfigration;
 import jedis.util.JedisObject;
 import jedis.util.MessageConstant;
@@ -12,8 +11,7 @@ public class SaveHandler implements CommandHandler {
 	public JedisObject execute(JedisClient client, CommandLine cl)
 			throws UnsupportedOperationException {
 		// TODO Auto-generated method stub
-		
-		cRDB.save(Server.databases, JedisConfigration.rdbPathName);
+		RDB.save(Server.inUseDatabases, JedisConfigration.rdbPathName);
 		return MessageConstant.OK;
 	}
 
