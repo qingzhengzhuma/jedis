b4fc7c47f92146926450b66a70c7c3bed903da26
liaojian <961617779@qq.com>
2016-09-30 23:06:54 +0800

    removed copy on write support

--- src/jedis/server/GetHandler.java
+++ src/jedis/server/GetHandler.java
@@ -1,7 +1,6 @@
 package jedis.server;
 
 import jedis.util.CommandLine;
-import jedis.util.JedisClient;
 import jedis.util.JedisObject;
 import jedis.util.Sds;
 
@@ -12,19 +11,7 @@ public class GetHandler implements CommandHandler {
 		// TODO Auto-generated method stub
 		int curDB = client.getCurrntDB();
 		Sds key = new Sds(cl.getArg(0));
-		
-		//once bufDataBases != null we know the rdb is saving
-		//background therefore we must search both databases,
-		//and because bufDatabases is always newer than databases,
-		//so we search bufDatabases first
-		if(Server.bufDatabases != null &&
-		   Server.bufDatabases[curDB].containsKey(key)){
-			return Server.bufDatabases[curDB].get(key);
-		}
-		if(Server.databases[curDB].containsKey(key)){
-			return Server.databases[curDB].get(key);
-		}
-		return null;
+		return Server.inUseDatabases[curDB].get(key);
 	}
 
 }
