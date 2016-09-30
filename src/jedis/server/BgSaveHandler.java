d0ab3415c4f523698eea9dfec9aad7d64a6d1951
liaojian <961617779@qq.com>
2016-09-30 23:02:18 +0800

    added replication before start RDB serialization.

--- src/jedis/server/BgSaveHandler.java
+++ src/jedis/server/BgSaveHandler.java
@@ -1,7 +1,6 @@
 package jedis.server;
 
 import jedis.util.CommandLine;
-import jedis.util.JedisClient;
 import jedis.util.JedisObject;
 import jedis.util.MessageConstant;
 
@@ -11,7 +10,14 @@ public class BgSaveHandler implements CommandHandler{
 	public JedisObject execute(JedisClient client, CommandLine cl) throws UnsupportedOperationException {
 		// TODO Auto-generated method stub
 		if(Server.aofSaveThread.isAlive()) return MessageConstant.AOF_BUSY;
-		Server.rdbSaveThread = new RdbSaveThread(Server.databases);
+		int dbNum = Server.inUseDatabases.length;
+		//Server.bufDatabases = Server.initDatabases(dbNum);
+		//Server.deletedDatabases = Server.initDatabases(dbNum);
+		JedisDB[] dbs = Server.initDatabases(dbNum);
+		for(int i = 0; i < dbNum;++i){
+			dbs[i] = Server.inUseDatabases[i].copy();
+		}
+		Server.rdbSaveThread = new RdbSaveThread(dbs);
 		Server.rdbSaveThread.start();
 		return MessageConstant.OK;
 	}
